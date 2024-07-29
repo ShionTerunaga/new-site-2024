51983eb10210f77d779039c2568517b4df907ede
ShionTerunaga <sion2769@gmail.com>
2024-07-29 23:44:02 +0900

    fix: fixed language

--- src/features/skills/view/skills-page.tsx
+++ src/features/skills/view/skills-page.tsx
@@ -1,3 +1,5 @@
+import CardCollection from "../components/card-collection/card-collection"
+import { allSkills } from "../static/skill-all"
 import { Header } from "@/components/header"
 import { en } from "@/shared/lang/en"
 
@@ -5,6 +7,9 @@ export const SkillsPage = () => {
     return (
         <main>
             <Header title={en.skills.title} />
+            {allSkills.map((item, index) => (
+                <CardCollection key={index} {...item} />
+            ))}
         </main>
     )
 }

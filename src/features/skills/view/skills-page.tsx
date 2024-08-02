7616d790ff261b06c9434a35371a7114a98658a6
teru12012000 <sion2769@gmail.com>
2024-08-02 09:26:40 +0900

    fix: fixation header

--- src/features/skills/view/skills-page.tsx
+++ src/features/skills/view/skills-page.tsx
@@ -1,5 +1,6 @@
 import CardCollection from "../components/card-collection/card-collection"
 import { allSkills } from "../static/skill-all"
+import styles from "./style.css"
 import { Header } from "@/components/header"
 import { en } from "@/shared/lang/en"
 
@@ -7,9 +8,11 @@ export const SkillsPage = () => {
     return (
         <main>
             <Header title={en.skills.title} />
-            {allSkills.map((item, index) => (
-                <CardCollection key={index} {...item} />
-            ))}
+            <div className={styles.container}>
+                {allSkills.map((item, index) => (
+                    <CardCollection key={index} {...item} />
+                ))}
+            </div>
         </main>
     )
 }

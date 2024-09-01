dda4ca45dfeefb992423636b8dbf33ed048febdf
teru12012000 <sion2769@gmail.com>
2024-09-01 14:11:59 +0900

    fix: omit error

--- src/features/activities/view/active.tsx
+++ src/features/activities/view/active.tsx
@@ -1,9 +1,6 @@
 import { CardView } from "@/components/view/cardView/view/cardView"
-import { pageInterface } from "@/service/interface/pageInterface"
 import { en } from "@/shared/lang/en"
 
 export const Activity = async () => {
-    const data = await pageInterface()
-
-    return <CardView pageTitle={en.active.title} contents={data} />
+    return <CardView pageTitle={en.active.title} contents={[]} />
 }

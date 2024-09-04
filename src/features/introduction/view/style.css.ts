659957bc487ca6870211d2b6ca2e6555b72824f7
teru12012000 <sion2769@gmail.com>
2024-09-05 00:49:03 +0900

    fix: change introduction

--- src/features/introduction/view/style.css.ts
+++ src/features/introduction/view/style.css.ts
@@ -7,6 +7,54 @@ const styles = {
     section: style({
         marginTop: 100
     }),
+    intro: style({
+        width: "75%",
+        borderRadius: 8,
+        margin: "auto",
+        boxShadow: "0 0 10px rgba(255,255,255,0.8)",
+        backgroundColor: "white",
+        padding: 25,
+        color: "black",
+        "@media": {
+            "screen and (max-width:430px)": {
+                paddingLeft: 0,
+                paddingRight: 0
+            },
+            "screen and (min-width:849px)": {
+                width: "50%",
+                height: 250,
+                display: "flex"
+            }
+        }
+    }),
+    titleBox: style({
+        fontSize: 18,
+        paddingBottom: 15,
+        "@media": {
+            "screen and (min-width:849px)": {
+                display: "flex",
+                paddingRight: 20,
+                justifyContent: "center",
+                alignItems: "center",
+                borderRight: "4px solid black",
+                height: 210
+            }
+        }
+    }),
+    introductionContents: style({
+        textAlign: "center",
+        fontSize: 18,
+        padding: 4,
+        "@media": {
+            "screen and (min-width:849px)": {
+                fontSize: 20,
+                textAlign: "left",
+                display: "flex",
+                alignItems: "center",
+                paddingLeft: 7
+            }
+        }
+    }),
     title: style({
         fontSize: 32
     }),

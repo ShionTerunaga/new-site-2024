047345da0ca922ca085f1d53fb9ac11ff7e2ad8a
ShionTerunaga <sion2769@gmail.com>
2024-09-11 23:13:59 +0900

    feat: card button

---
+++ src/features/skills/components/cardView/card-button/card-button.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { StaticImageData } from "next/image"
+import { ReactNode } from "react"
+import styles from "./style.css"
+import { Card } from "@/components/ui/card"
+import { PopupContents } from "@/components/ui/popup"
+import { usePopup } from "@/hooks/popup"
+
+interface props {
+    /** カードのタイトル */
+    title: string
+    /** カードの画像 */
+    image: StaticImageData
+    /**ポップアップの中身 */
+    children: ReactNode
+}
+
+const CardButton = (props: props) => {
+    const { openPopup } = usePopup()
+
+    const handleClick = () => {
+        openPopup(<PopupContents {...props} />)
+    }
+
+    return (
+        <button onClick={handleClick} className={styles.container}>
+            <Card title={props.title} image={props.image} description="" />
+        </button>
+    )
+}
+
+export default CardButton
---
+++ src/features/skills/components/cardView/card-button/style.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css"
+
+const styles = {
+    container: style({
+        width: 202,
+        height: 252,
+        padding: 0,
+        border: "none",
+        backgroundColor: "transparent",
+        cursor: "pointer"
+    })
+}
+
+export default styles

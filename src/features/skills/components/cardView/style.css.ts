1462ff86ffd1637cfa3ecd9d12264ec2e1950a0f
ShionTerunaga <sion2769@gmail.com>
2024-09-12 02:17:51 +0900

    fix: delete overflow

--- src/features/skills/components/cardView/style.css.ts
+++ src/features/skills/components/cardView/style.css.ts
@@ -1,10 +1,3 @@
-import { style } from "@vanilla-extract/css"
-
-const styles = {
-    container: style({
-        marginTop: 70,
-        overflow: "auto"
-    })
-}
+const styles = {}
 
 export default styles

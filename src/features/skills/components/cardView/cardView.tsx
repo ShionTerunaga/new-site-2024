ba08f3c8877880e23aba5854b4afdddcf06f3eba
teru12012000 <sion2769@gmail.com>
2024-09-12 02:17:51 +0900

    fix: delete overflow

--- src/features/skills/components/cardView/cardView.tsx
+++ src/features/skills/components/cardView/cardView.tsx
@@ -1,6 +1,5 @@
 import CardCollection from "./card-collection/card-collection"
 import { contentsAll } from "./card-collection/contents-all.type"
-import styles from "./style.css"
 import { Header } from "@/components/ui/header"
 
 interface props {
@@ -12,7 +11,7 @@ export const CardView = (props: props) => {
     return (
         <section>
             <Header title={props.pageTitle} />
-            <div className={styles.container}>
+            <div>
                 {Array.isArray(props.contents) ? (
                     <>
                         {props.contents.map((item, index) => (

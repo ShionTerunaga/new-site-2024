a2861abe2fe46e160b353015eb53b4bb7572314c
teru12012000 <sion2769@gmail.com>
2024-09-15 05:48:02 +0900

    feat: add activity

--- src/features/activities/view/active.tsx
+++ src/features/activities/view/active.tsx
@@ -1,6 +1,35 @@
-import { CardView } from "@/features/skills/components/cardView/cardView"
+import Link from "next/link"
+import styles from "./style.css"
+import { Header } from "@/components/ui/header"
+import { getAllContents } from "@/services/lib"
 import { en } from "@/shared/lang/en"
 
-export const Activity = async () => {
-    return <CardView pageTitle={en.active.title} contents={[]} />
+export const Activity = () => {
+    const contents = getAllContents()
+
+    return (
+        <>
+            <Header title="activity" />
+            <main className={styles.container}>
+                <h1 className={styles.title}>{en.active.title}</h1>
+                <section className={styles.section}>
+                    <ol>
+                        {contents.map((item, index) => (
+                            <li key={index}>
+                                <Link
+                                    href={`/activities/${item.id}`}
+                                    className={styles.link}
+                                >
+                                    <h2>{item.title}</h2>
+                                    <p className={styles.description}>
+                                        {item.description}
+                                    </p>
+                                </Link>
+                            </li>
+                        ))}
+                    </ol>
+                </section>
+            </main>
+        </>
+    )
 }

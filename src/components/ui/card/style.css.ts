8a3c698a2b5d4a5ae09c77d4e6a95e314107ea9f
ShionTerunaga <sion2769@gmail.com>
2024-10-03 02:29:03 +0900

    delete comments

--- src/components/ui/card/style.css.ts
+++ src/components/ui/card/style.css.ts
@@ -2,14 +2,9 @@ import { style } from "@vanilla-extract/css"
 
 const styles = {
     box: style({
-        // width: 202,
-        // height: 252,
         backgroundColor: "#333",
-        // border: "1px solid silver",
         borderRadius: 5,
         color: "#fff",
-        // boxShadow: "0 0 10px white",
-
         padding: "1em",
         height: '100%',
         transition: "0.4s",
@@ -17,10 +12,6 @@ const styles = {
         ":hover": {
             boxShadow: "0 0 15px 4px skyBlue",
         }
-    }),
-    picture: style({
-        // width: "100%",
-        // height: 125
     }),
     image: style({
         borderStartStartRadius: 3,
@@ -34,13 +25,7 @@ const styles = {
         width: "100%",
         objectFit: 'contain'
     }),
-    textBox: style({
-        // width: "100%",
-        // height: 125
-    }),
     noneContents: style({
-        // width: "100%",
-        // height: 125,
         display: "flex",
         marginTop: 30,
         justifyContent: "center"
@@ -50,13 +35,10 @@ const styles = {
         textAlign: 'center'
     }),
     descriptionBox: style({
-        // width: 200,
-        // height: 70,
         borderBottomLeftRadius: 4,
         borderBottomRightRadius: 4
     }),
     description: style({
-        // color: "black",
         marginTop: 10,
         overflowWrap: "break-word"
     })
--- src/features/home/view/home.tsx
+++ src/features/home/view/home.tsx
@@ -13,30 +13,26 @@ import { en } from "@/shared/lang/en"
 export const Home = () => {
     return (
         <main className={styles.container}>
-            {/* <div> */}
-                <section className={styles.iconBox}>
-                    <Icon />
-                </section>
-            {/* </div> */}
-            {/* <div> */}
-                <section className={styles.greetingBox}>
-                    <h1 className={styles.name}>{en.home.name}</h1>
-                    <div className={styles.descriptionBox}>
-                        <p>{en.home.description}</p>
-                    </div>
-                    <div className={styles.icons}>
-                        <Link href="https://x.com/TERUSI1201" target="_blank">
-                            <Image src={X} width={20} height={20} alt="" />
-                        </Link>
-                        <Link
-                            href="https://github.com/ShionTerunaga"
-                            target="_blank"
-                        >
-                            <Image src={GITHUB} width={22} height={22} alt="" />
-                        </Link>
-                    </div>
-                </section>
-            {/* </div> */}
+            <section className={styles.iconBox}>
+                <Icon />
+            </section>
+            <section className={styles.greetingBox}>
+                <h1 className={styles.name}>{en.home.name}</h1>
+                <div className={styles.descriptionBox}>
+                    <p>{en.home.description}</p>
+                </div>
+                <div className={styles.icons}>
+                    <Link href="https://x.com/TERUSI1201" target="_blank">
+                        <Image src={X} width={20} height={20} alt="" />
+                    </Link>
+                    <Link
+                        href="https://github.com/ShionTerunaga"
+                        target="_blank"
+                    >
+                        <Image src={GITHUB} width={22} height={22} alt="" />
+                    </Link>
+                </div>
+            </section>
             <section className={styles.cardContainer}>
                 {staticCard.map((item, index) => (
                     <CardContainer index={index} item={item} key={index} />

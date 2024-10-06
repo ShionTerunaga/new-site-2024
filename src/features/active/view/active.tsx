783b293d1b59360c2d64d0ed9e8ac137b3986eca
teru12012000 <sion2769@gmail.com>
2024-10-06 15:37:33 +0900

    fix: delete markdown

--- src/features/active/view/active.tsx
+++ src/features/active/view/active.tsx
@@ -1,8 +1,5 @@
-import { MDXRemote } from "next-mdx-remote/rsc"
 import styles from "./style.css"
 import { Header } from "@/components/ui/header"
-import { getContents } from "@/services/lib"
-import { markdownComponent } from "@/services/markdown"
 import { en } from "@/shared/lang/en"
 import { routingPath } from "@/shared/static/routingPaths"
 
@@ -13,7 +10,7 @@ interface props {
 
 export const Active = async (props: props) => {
     //const t = i18n(props.lang)
-    const markdown = getContents(props.id, props.lang)
+    //const markdown = getContents(props.id, props.lang)
 
     return (
         <>
@@ -25,14 +22,14 @@ export const Active = async (props: props) => {
             <main className={styles.mainBox}>
                 <section className={styles.container}>
                     <h1 className={styles.heading}>
-                        {markdown.overview.title}
+                        {/*markdown.overview.title*/}
                     </h1>
                     <section className={styles.contents}>
-                        <MDXRemote
+                        {/*<MDXRemote
                             source={markdown.source}
                             options={markdown.options}
                             components={markdownComponent}
-                        />
+                        />*/}
                     </section>
                 </section>
             </main>

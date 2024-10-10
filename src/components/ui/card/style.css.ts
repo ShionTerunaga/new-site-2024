61dfd036b7eca30853bf4b9425bdf5a5a9c8371e
kasumi0 <parfumdelapeche@gmail.com>
2024-10-10 14:22:33 +0900

    fix/design

--- src/components/ui/card/style.css.ts
+++ src/components/ui/card/style.css.ts
@@ -1,32 +1,23 @@
 import { style } from "@vanilla-extract/css"
 
-export const box = style({
-    backgroundColor: "#333",
-    borderRadius: 5,
-    color: "#fff",
-    padding: "1em",
-    height: "100%",
-    transition: "box-shadow 0.4s",
-    ":hover": {
-        boxShadow: "0 0 15px 4px skyBlue"
-    }
-})
 const styles = {
+     box: style({
+        backgroundColor: "#333",
+        borderRadius: 5,
+        color: "#fff",
+        padding: "1em",
+        height: "100%",
+        transition: "box-shadow 0.4s",
+        ":hover": {
+            boxShadow: "0 0 15px 4px skyBlue"
+        }
+    }),
     isSkills: style({
         display: "grid",
         placeItems: "center",
         placeContent: "center",
-        transitionProperty: "box-shadow, rotate",
-        transitionDuration: ".4s",
-        transformStyle: "preserve-3d",
-        perspective: "100vmax",
-        ":hover": {
-            rotate: ".4 1 0 30deg"
-        }
     }),
     image: style({
-        borderStartStartRadius: 3,
-        borderStartEndRadius: 3,
         pointerEvents: "none",
         WebkitTouchCallout: "none",
         WebkitUserSelect: "none",
@@ -38,16 +29,6 @@ const styles = {
         marginInline: "auto",
         objectFit: "contain"
     }),
-    isSkillsImg: style({
-        transitionProperty: "translate, rotate",
-        transitionDuration: ".4s",
-        selectors: {
-            [`${box}:hover &`]: {
-                translate: "14px -4px 80px",
-                rotate: ".4 1 0 10deg"
-            }
-        }
-    }),
     noneContents: style({
         display: "flex",
         marginTop: 10,
@@ -58,14 +39,6 @@ const styles = {
         textAlign: "center"
     }),
     isSkillsH2: style({
-        transitionProperty: "translate, filter, rotate",
-        transitionDuration: ".4s",
-        ":hover": {
-            translate: "14px -4px 80px",
-            rotate: ".4 1 0 10deg",
-            filter: "drop-shadow(-8px 3px 10px skyBlue) drop-shadow(-20px 5px 16px skyBlue)"
-        },
-
         "@media": {
             "(width < 870px)": {
                 fontSize: "max(2.6vw, 1.12em)"

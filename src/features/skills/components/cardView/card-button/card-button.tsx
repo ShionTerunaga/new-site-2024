c0c228e5a8bbaef68bd4a3a1955ad99e55ad0c14
kasumi0 <parfumdelapeche@gmail.com>
2024-10-11 23:58:05 +0900

    fix/fix/fix/design

--- src/features/skills/components/cardView/card-button/card-button.tsx
+++ src/features/skills/components/cardView/card-button/card-button.tsx
@@ -25,7 +25,7 @@ const CardButton = (props: props) => {
 
     return (
         <button onClick={handleClick} className={styles.container}>
-            <Card title={props.title} image={props.image} description="" isSkills/>
+            <Card description="" isSkills {...props} />
         </button>
     )
 }

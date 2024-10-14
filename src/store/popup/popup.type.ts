1bc4c6a6bc9808a4c3b27bc7d1d2e663a395f416
teru12012000 <sion2769@gmail.com>
2024-10-15 00:31:28 +0900

    fix: fixed popup type

--- src/store/popup/popup.type.ts
+++ src/store/popup/popup.type.ts
@@ -1,14 +1,26 @@
 import { Dispatch, ReactNode } from "react"
 
-export type popState = {
-    state: boolean
-    children?: ReactNode
+export type openState = {
+    state: true
+    children: ReactNode
 }
 
-export type popAction = {
-    type: "show" | "hidden"
-    children?: ReactNode
+export type closeState = {
+    state: false
 }
+
+export type popState = closeState | openState
+
+export type openAction = {
+    type: "show"
+    children: ReactNode
+}
+
+export type closeAction = {
+    type: "hidden"
+}
+
+export type popAction = closeAction | openAction
 
 export type popupContextType = {
     state: popState

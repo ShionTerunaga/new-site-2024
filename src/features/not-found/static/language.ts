feab5a40253d19ab282c605637f3f759e32e255c
teru12012000 <sion2769@gmail.com>
2024-12-16 04:36:38 +0900

    feat: add page of not found

---
+++ src/features/not-found/static/language.ts
@@ -0,0 +1,15 @@
+export type selectLanguage = {
+    lang: string
+    text: string
+}
+
+export const languages: selectLanguage[] = [
+    {
+        lang: "en",
+        text: "English ver"
+    },
+    {
+        lang: "ja",
+        text: "Japanese ver"
+    }
+]

40ab9c6186ad342403f0d86b7179d69d25283270
ShionTerunaga <sion2769@gmail.com>
2025-01-29 00:56:30 +0900

    fixed animation

--- src/features/top/components/title-section/title-section.tsx
+++ src/features/top/components/title-section/title-section.tsx
@@ -1,51 +1,64 @@
 "use client"
 
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, motion } from "motion/react"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from "./style.css"
 import { routingPath } from "@/shared/static/routingPaths"
 import { topAnimation } from "@/shared/static/topAnimation"
+import { sleep } from "@/utils/sleep"
 
 const TitleSection = () => {
     const router = useRouter()
     const [counter, setCounter] = useState<number>(0)
     const [flag, setFlag] = useState<boolean>(false)
+    const animationCounterRef = useRef<number>(0)
 
     useEffect(() => {
-        if (counter === 0) setFlag(true)
+        setFlag(true)
 
-        if (counter < topAnimation.length) {
-            setTimeout(() => {
-                setFlag(false)
+        setTimeout(() => {
+            setFlag(false)
+            setCounter(counter + 1)
+        }, 3000)
+    }, [])
 
-                setCounter(counter + 1)
+    const animationComplete = async () => {
+        animationCounterRef.current += 1
 
-                setFlag(true)
-            }, 3000)
+        if (animationCounterRef.current === 1) return
+
+        if (animationCounterRef.current % 2 === 1) {
+            await sleep(3000)
+
+            setFlag(false)
+
+            return
         }
 
-        if (counter === topAnimation.length) {
-            setTimeout(() => {
-                setFlag(false)
-
-                router.push(routingPath("en").home)
-            }, 500)
+        if (animationCounterRef.current === 6) {
+            router.push(routingPath("en").home)
         }
-    }, [counter, router])
+
+        setCounter(counter + 1)
+
+        setFlag(true)
+    }
 
     return (
         <section className={styles.titleBox}>
             <AnimatePresence>
-                {flag && (
+                {flag ? (
                     <motion.h1
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
                         transition={{ delay: 0.5 }}
+                        onAnimationComplete={animationComplete}
                     >
                         {topAnimation[counter]}
                     </motion.h1>
-                )}
+                ) : null}
             </AnimatePresence>
         </section>
     )

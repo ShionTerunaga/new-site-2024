17a601a9ec1bb315056003d2bb2ae0729ebf7395
teru12012000 <sion2769@gmail.com>
2025-02-12 23:44:31 +0900

    feat: result

--- src/features/popup/hooks/base/useBase.tsx
+++ src/features/popup/hooks/base/useBase.tsx
@@ -1,10 +1,11 @@
 import { useContext } from "react"
 import { PopupContext } from "../../store/popup-context"
+import { RESULT_ERROR } from "@/utils/others"
 
 const useBase = () => {
     const context = useContext(PopupContext)
 
-    if (context.kind === "error") {
+    if (context.kind === RESULT_ERROR) {
         throw context.err
     }
 

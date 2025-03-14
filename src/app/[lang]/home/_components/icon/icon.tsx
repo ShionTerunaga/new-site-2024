de48159368c8dadcc09a40397420dd9f58828b36
ShionTerunaga <sion2769@gmail.com>
2025-03-14 22:10:54 +0900

    fix: move icon

--- src/app/[lang]/home/_components/icon/icon.tsx
+++ src/app/[lang]/home/_components/icon/icon.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
+import { MY_FACE } from "../../_static/assets"
 import styles from "./style.css"
-import ICON from "@/assets/home/icon.jpg"
 
 const Icon = () => {
     return (
         <picture>
             <Image
-                src={ICON}
+                src={MY_FACE}
                 width={100}
                 height={100}
                 alt="My Icon"
---
+++ src/app/[lang]/home/_static/assets.ts
@@ -0,0 +1,3 @@
+import ICON from "@/assets/home/icon.jpg"
+
+export const MY_FACE = ICON

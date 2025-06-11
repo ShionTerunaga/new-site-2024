49121c825a1ffafb14e32825165375fd2fb82536
ShionTerunaga <sion2769@gmail.com>
2025-06-11 20:58:33 +0900

    fix: link card

--- src/features/markdown/template/view/a/services/get-url-contents.ts
+++ src/features/markdown/template/view/a/services/get-url-contents.ts
@@ -1,12 +1,20 @@
 import { JSDOM } from "jsdom";
 import { LinkDict } from "./get-url-contents.type";
+import { createResult, Result } from "@/utils/result";
 
-export const getUrlContents = async (url: string): Promise<LinkDict> => {
+export const getUrlContents = async (
+    url: string
+): Promise<Result<LinkDict, string>> => {
     const res = await fetch(url);
 
     if (!res.ok) {
+        if (res.status === 429) {
+            return createResult.err(url);
+        }
+
         throw new Error("response is failed");
     }
+
     const html = await res.text();
 
     const replaceHTML = html
@@ -18,20 +26,20 @@ export const getUrlContents = async (url: string): Promise<LinkDict> => {
     const twitterPost = url.match(/https:\/\/x\.com\/([\w]+)\/status\/([\d]+)/);
 
     if (twitterPost) {
-        return {
+        return createResult.ok({
             url: url
-        };
+        });
     }
 
     const twitterHome = url.match(/^https?:\/\/x\.com\/([\w]+)/);
 
     //ここだけはnullは仕方ない
     if (twitterHome !== null) {
-        return {
+        return createResult.ok({
             "og:title": url,
             "og:image":
                 "https://abs.twimg.com/responsive-web/client-web/icon-ios.77d25eba.png"
-        };
+        });
     }
 
     const meta = DOM.window.document.head.querySelectorAll("meta");
@@ -64,5 +72,5 @@ export const getUrlContents = async (url: string): Promise<LinkDict> => {
         dict["og:title"] = meta[0].textContent;
     }
 
-    return dict;
+    return createResult.ok(dict);
 };
